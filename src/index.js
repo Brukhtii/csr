3fe5ee43133dc6a543c358bf2b701dbdcb20004a
Serhii Brukhtii <sergey.brukhtii@gmail.com>
2024-06-08 20:16:22 +0300

    Ready CSR to deploy

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./style.css";
+import { BrowserRouter } from 'react-router-dom'
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
